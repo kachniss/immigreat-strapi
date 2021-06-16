50d63bb35ddb32ecb012c1ad0ac6c6b6ba93462c
Kachniss <vopalkova.katerina@gmail.com>
2021-06-15 22:13:06 -0700

    Initialize Strapi

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '86bb306ce59d7a36f69267b98ad20fcc'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '560c4088-bc85-4cdd-affb-7967d696cf70'
+};
